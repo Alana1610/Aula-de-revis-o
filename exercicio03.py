8e0aefc9b7494e044f4ce02f71764b8ab93bcdac
AlanaMaria <mariaalana730@gmail.com>
2025-04-22 15:48:23 -0300

    exercicio04

---
+++ exercicio03.py
@@ -0,0 +1,5 @@
+numero= int(input("Digite um numero inteiro:"))
+antecessor = numero -1
+sucessor = numero +1
+print(f"O antecessor do {numero} é o {antecessor}")
+print(f"O sucessor do {numero} é o {sucessor}")
---
+++ exercicio04.py
@@ -0,0 +1,4 @@
+salario=float(input("Digite  o valor do salario minimo:"))
+salario_usuario=float(input("Digite seu salario:"))
+quanto=salario_usuario/salario
+print(f"{quanto:.2f}")
