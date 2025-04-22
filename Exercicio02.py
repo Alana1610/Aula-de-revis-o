fb6c93510c1e07684cfbd1f60be72531dc1f5891
AlanaMaria <mariaalana730@gmail.com>
2025-04-22 15:04:50 -0300

    exercicio02

--- Exercicio02.py
+++ Exercicio02.py
@@ -1,9 +1,15 @@
-numero= int(input("digite um numero:"))
-if numero >0 and numero %2 ==0:
-    print(f"O numero é positivo e par")
-elif numero >0 and numero %2 ==0 :
-    print(f"O numero é negativo e par")
-elif numero <0 and numero %2 ==0 :
-    print(f"O numero é positivo e impar")
-else:
-    print(f"O numero é negativo e impar")
+
+while True:
+    numero= int(input("digite um numero:"))
+    if numero >0 and numero %2 ==0:
+        print(f"O numero é positivo e par")
+    elif numero >0 and numero %2 ==0 :
+        print(f"O numero é negativo e par")
+    elif numero <0 and numero %2 ==0 :
+        print(f"O numero é positivo e impar")
+    else:
+        print(f"O numero é negativo e impar")
+        continuar= input("Deseja tentar com um novo numero?(s/n):").lower()
+        if continuar != 's':
+            print(f"Encerrando o programa")
+            break
