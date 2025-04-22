fa3cc9eb800cefc4b180358e3e4a337fbf570646
AlanaMaria <mariaalana730@gmail.com>
2025-04-22 14:33:09 -0300

    exercicios de revisao

---
+++ Exercicio02.py
@@ -0,0 +1,9 @@
+numero= int(input("digite um numero:"))
+if numero >0 and numero %2 ==0:
+    print(f"O numero é positivo e par")
+elif numero >0 and numero %2 ==0 :
+    print(f"O numero é negativo e par")
+elif numero <0 and numero %2 ==0 :
+    print(f"O numero é positivo e impar")
+else:
+    print(f"O numero é negativo e impar")
---
+++ exercicio01.py
@@ -0,0 +1,7 @@
+a= float(input("Insira o valor de A:"))
+b= float(input("Insira o valor de B:"))
+c= float(input("Insira o valor de C:"))
+soma= a+b
+print(f"O valor da soma é,{soma}")
+if soma <c:
+    print(f"A soma é maior que C")
